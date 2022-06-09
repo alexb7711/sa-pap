f243c6dac85aea942734890d1184c62b3c607534
Alexander Brown <alex.brown@brownhaus.xyz>
2022-06-08 19:53:10 -0600

    Outline code and working on uml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+// Public modules
+
+// My modules
+pub mod plotter;            // Plot solution data
+pub mod sa;                 // Simulated annealing algorithm
+pub mod schedule_generator; // Create a random bus schedule
+pub mod solution_generator; // Generate a solution to the bus schedule
+pub mod solution_tweaker;   // Tweak a given solution
+
+//===============================================================================
+//
+fn main()
+{
+    // Create simulated annealing module
+    //   - Select initial temperature
+    //   - Select cooling schedule
+
+    // Create schedule generator
+
+    // Create solution generator and tweaker
+
+    // Pass schedule generator, solution generator, and solution tweaker
+    // into the SA module
+
+    // Run simulated annealing simulation
+
+    // Plot results
 }
