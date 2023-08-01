d0ae2c0eff5a25343033a82016751e9f9347abd4
Alexander Brown <alex.brown7711@gmail.com>
2023-07-31 20:42:25 -0600

    Added initializer to class

--- src/sa/route/route_csv.rs
+++ src/sa/route/route_csv.rs
@@ -1,13 +1,15 @@
 //===============================================================================
 // Declare submodules
+pub use crate::sa::route::route_rand_generator::route_event::RouteEvent;        // Keep public for testing
+use crate::sa::route::Route;
+use crate::util::fileio::yaml_loader;
+use yaml_rust::Yaml;
 
 //===============================================================================
 // External Crates
 
 //===============================================================================
 // Import Crates
-pub use crate::sa::route::route_rand_generator::route_event::RouteEvent;     // Keep public for testing
-use crate::sa::route::Route;
 
 //===============================================================================
 // Import modules
@@ -21,6 +23,38 @@ pub struct RouteCSVGenerator
     // PUBLIC
 
     // PRIVATE
+    config: Yaml,
+    load_from_file: bool,
+}
+
+//===============================================================================
+// Implementation of ScheduleCSVGenerator
+impl RouteCSVGenerator
+{
+    //===========================================================================
+    // PUBLIC
+
+    //---------------------------------------------------------------------------
+    /// Returns a schedule generator
+    ///
+    /// # Input
+    /// * `config_path`: Path to YAML schedule config
+    ///
+    /// # Output
+    /// * `ScheduleGenerator`
+    ///
+    pub fn new(load_from_file: bool, config_path: &str) -> RouteCSVGenerator
+    {
+        // Create new RouteGenerator
+        let rg = RouteCSVGenerator
+        {
+            config: yaml_loader::load_yaml(config_path),
+            load_from_file,
+        };
+
+        // Return Route Generator
+        return rg;
+    }
 }
 
 //===============================================================================
