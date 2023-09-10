57779ddf630e46bca5eabc819a0d1308f84256c0
Alexander Brown <alex.brown7711@gmail.com>
2023-09-10 14:45:22 -0600

    Create new charger prmitive

--- src/sa/generators/primitives/new_charger.rs
+++ src/sa/generators/primitives/new_charger.rs
@@ -3,16 +3,25 @@
 //
 mod new_charger {
 
+    // Import modules
+    use crate::sa::charger::Charger;
+
     //--------------------------------------------------------------------------
     /// The run function executes the `new_charger` module. Given the set of routes and a bus ID and visit, the run
-    /// function shall assign a bus to an available charger and return the new schedule.
+    /// function shall assign a bus to an available charger and return the new schedule. Return false if assignment
+    // failed and true if successful.
     ///
     /// # Input
-    /// * NONE
+    /// * ch: Charger object
+    /// * q: Queue wanting to be assigned to
+    /// * ts: Time slice to assign to queue
+    /// * b: Bus id
     ///
     /// # Output
-    /// * NONE
+    /// * bool: Assignment failure/success
     ///
     #[allow(dead_code)]
-    pub fn run() {}
+    pub fn run(ch: &mut Charger, q: usize, ts: (f32, f32), b: usize) -> bool {
+        ch.assign(q, ts, b)
+    }
 }
