652b7dc781df524486ab10c971021027af83f1a3
Alexander Brown <alex.brown7711@gmail.com>
2023-10-15 17:11:42 -0600

    Writing tweaks

--- src/sa/generators/tweak_schedule.rs
+++ src/sa/generators/tweak_schedule.rs
@@ -3,6 +3,34 @@
 use crate::sa::charger::Charger;
 use crate::sa::route::Route;
 use crate::sa::generators::Generator;
+use rand::distributions::{Distribution, Standard};
+use rand::Rng;
+use crate::sa::generators::primitives::new_charger::*;
+use crate::sa::generators::primitives::new_visit::*;
+use crate::sa::generators::primitives::new_window::*;
+use crate::sa::generators::primitives::remove::*;
+use crate::sa::generators::primitives::slide_visit::*;
+
+//===============================================================================
+/// Structure defining the information to create a charge schedule
+//
+enum Primitives {
+    NewCharger, NewWindow, SlideVisit
+}
+
+
+//===============================================================================
+/// Implementation of `Distribution` for `Primitives` enum
+//
+impl Distribution<Primitives> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Primitives {
+        match rng.gen_range(0..2) {
+            0 => Primitives::NewCharger,
+            1 => Primitives::NewWindow,
+            _ => Primitives::SlideVisit,
+        }
+    }
+}
 
 //===============================================================================
 /// Structure defining the information to create a charge schedule
@@ -14,6 +42,7 @@ pub struct TweakSchedule {}
 /// Implementation of `TweakSchedule`
 //
 impl TweakSchedule {
+
     //---------------------------------------------------------------------------
     /// Initialize the `TweakSchedule` object
     ///
@@ -32,5 +61,19 @@ impl TweakSchedule {
 /// Implementation of `Generator` for `TweakSchedule`
 //
 impl Generator for TweakSchedule {
-    fn run(self: &mut TweakSchedule, _s: &mut dyn Route, _c: &mut Charger) {}
+    fn run(self: &mut TweakSchedule, _s: &mut dyn Route, c: &mut Charger) -> bool {
+        let p: Primitives = rand::random();
+
+        match p {
+            Primitives::NewCharger => {
+                new_charger::run(c, 0, 0, &(0.0, 0.0))
+            },
+            Primitives::NewWindow  => {
+                new_window::run(c, 0, &(0.0, 0.0), &(0.0, 0.0))
+            },
+            Primitives::SlideVisit => {
+                slide_visit::run(c, 0, 0, &(0.0, 0.0), &(0.0, 0.0))
+            },
+        }
+    }
 }
