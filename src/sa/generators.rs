593f1ab4039327be01e97d9d7c152472ff50c652
Alexander Brown <alex.brown7711@gmail.com>
2023-10-17 07:53:22 -0600

    Update generator names

--- src/sa/generators.rs
+++ src/sa/generators.rs
@@ -1,6 +1,7 @@
 // Generators
+pub mod gen_new_visits; // Create new charge schedule with new_visits primitive
+pub mod gen_wait_queue; // Create new charge schedule by placing in waiting queues
 pub mod primitives;
-pub mod schedule_generator; // Create charge schedule
 pub mod tweak_schedule; // Alter a charge schedule // Pool of all the SA generator primitives
 
 //===============================================================================
--- tests/test_generators.rs
+++ tests/test_generators.rs
@@ -8,10 +8,10 @@ mod test_generators {
     //---------------------------------------------------------------------------
     // Import modules
     use super::sa_pap::sa::charger::Charger;
+    use super::sa_pap::sa::generators::gen_wait_queue::GenWaitQueue;
+    use super::sa_pap::sa::generators::Generator;
     use super::sa_pap::sa::route::route_csv_generator::RouteCSVGenerator;
     use super::sa_pap::sa::route::Route;
-    use super::sa_pap::sa::generators::Generator;
-    use super::sa_pap::sa::generators::schedule_generator::ScheduleGenerator;
 
     //---------------------------------------------------------------------------
     //
@@ -19,7 +19,7 @@ mod test_generators {
         return "./src/config/schedule-test.yaml";
     }
 
-   //---------------------------------------------------------------------------
+    //---------------------------------------------------------------------------
     //
     fn csv_path() -> &'static str {
         return "./src/config/routes.csv";
@@ -37,7 +37,7 @@ mod test_generators {
         rg.run();
 
         // Create the generator
-        let mut sg = ScheduleGenerator::new();
+        let mut sg = GenWaitQueue::new();
 
         // Run the generator
         sg.run(&mut rg, &mut charger);
@@ -46,16 +46,16 @@ mod test_generators {
         let _id: usize = 0;
 
         // Test 0 - Check first index of a few chargers
-        assert_eq!(charger.schedule[0][0].t, (0.0,0.0));
+        assert_eq!(charger.schedule[0][0].t, (0.0, 0.0));
         assert_eq!(charger.schedule[0][0].b, 0);
 
-        assert_eq!(charger.schedule[1][0].t, (0.0,0.0));
+        assert_eq!(charger.schedule[1][0].t, (0.0, 0.0));
         assert_eq!(charger.schedule[1][0].b, 1);
 
-        assert_eq!(charger.schedule[2][0].t, (0.0,0.0));
+        assert_eq!(charger.schedule[2][0].t, (0.0, 0.0));
         assert_eq!(charger.schedule[2][0].b, 2);
 
-        assert_eq!(charger.schedule[9][0].t, (0.0,0.0));
+        assert_eq!(charger.schedule[9][0].t, (0.0, 0.0));
         assert_eq!(charger.schedule[9][0].b, 9);
 
         // Test 1 - Test first index of a few chargers
@@ -66,11 +66,10 @@ mod test_generators {
         assert_eq!(charger.schedule[1][1].t, (5.6666665, 5.6666665));
         assert_eq!(charger.schedule[1][1].b, 1);
 
-        assert_eq!(charger.schedule[2][1].t, (6.0,6.0));
+        assert_eq!(charger.schedule[2][1].t, (6.0, 6.0));
         assert_eq!(charger.schedule[2][1].b, 2);
 
-        assert_eq!(charger.schedule[18][1].t, (6.0,6.375));
+        assert_eq!(charger.schedule[18][1].t, (6.0, 6.375));
         assert_eq!(charger.schedule[18][1].b, 18);
-
     }
 }
