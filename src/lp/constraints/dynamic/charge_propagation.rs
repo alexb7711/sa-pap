33803f662bbe0823d44c331b36bbe8bac09b32d4
Alexander Brown <alex.brown7711@gmail.com>
2023-12-02 15:54:08 -0700

    test update charge

--- src/lp/constraints/dynamic/charge_propagation.rs
+++ src/lp/constraints/dynamic/charge_propagation.rs
@@ -38,15 +38,22 @@ impl Constraint for ChargePropagate {
         // Extract decision variables
         let eta = &mut d.dec.eta;
         let w = &d.dec.w;
+        let s = &mut d.dec.s;
 
         // Constraint
 
         // Calculate charge amount
-        let charge: f32 = (0..Q).map(|q| f32::from(w[i][q]) * r[q]).sum();
+        let mut charge: f32 = (0..Q).map(|q| f32::from(w[i][q]) * r[q] * s[i]).sum();
 
         // Ensure the charge does not exceed the battery limit
         if !(eta[i] + charge <= kappa[Gam[i] as usize]) {
-            return false;
+            // Adjust charge times
+            let charge_rate: f32 = (0..Q).map(|q| f32::from(w[i][q]) * r[q]).sum();
+            s[i] = (kappa[Gam[i] as usize] - eta[i]) / charge_rate;
+
+            // Update the charge
+            charge = (0..Q).map(|q| f32::from(w[i][q]) * r[q] * s[i]).sum();
+            // println!("Charge: {}", charge);
         }
 
         // Ensure the charge does not go below the minimum allowed threshold
