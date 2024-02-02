b9a083e998460db93a4f7ecad4b9381870b00b4e
Alexander Brown <alex.brown7711@gmail.com>
2024-02-02 09:50:58 -0700

    Add test and try to make test a bit more robust
    If the tweaker moves a "wait" to a "wait" for the same visit, it won't see any changes to the data and will fail the test

--- tests/test_tweak_schedule.rs
+++ tests/test_tweak_schedule.rs
@@ -10,6 +10,7 @@ mod test_tweak_schedule {
     use super::sa_pap::sa::charger::Charger;
     use super::sa_pap::sa::generators::gen_new_visits::GenNewVisits;
     use super::sa_pap::sa::generators::tweak_schedule::TweakSchedule;
+    use super::sa_pap::sa::generators::tweak_schedule::TweakScheduleQuick;
     use super::sa_pap::sa::generators::Generator;
     use super::sa_pap::sa::route::route_csv_generator::RouteCSVGenerator;
     use super::sa_pap::sa::route::Route;
@@ -58,7 +59,52 @@ mod test_tweak_schedule {
         for _ in 0..10 {
             //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             // Tweak the original schedule and check for updates
-            while !gtweak.run(&mut gsys, &mut charger) {}
+            for _ in 0..4 {
+                while !gtweak.run(&mut gsys, &mut charger) {}
+            }
+
+            let sol_new = gsys.get_data().dec;
+
+            assert_ne!(sol_orig, sol_new, "The old and new solution match");
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    //
+    #[test]
+    fn test_tweak_schedule_quick_result() {
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Initialize
+        // Load charger parameters from YAML file
+        let mut charger: Charger = Charger::new(yaml_path(), true, Some(35), None);
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Generate schedule
+        let mut gsys: Box<RouteCSVGenerator> =
+            Box::new(RouteCSVGenerator::new(yaml_path(), csv_path()));
+        gsys.run();
+        let mut gsys: Box<dyn Route> = gsys;
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Create solution generator and run it
+        let mut gsol: Box<GenNewVisits> = Box::new(GenNewVisits::new());
+        gsol.run(&mut gsys, &mut charger);
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Get a copy of the solution
+        let sol_orig = gsys.get_data().dec;
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Create tweaker
+        let mut gtweak: Box<TweakScheduleQuick> = Box::new(TweakScheduleQuick::new());
+
+        for _ in 0..10 {
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            // Tweak the original schedule and check for updates
+            for _ in 0..4 {
+                while !gtweak.run(&mut gsys, &mut charger) {}
+            }
+
             let sol_new = gsys.get_data().dec;
 
             assert_ne!(sol_orig, sol_new, "The old and new solution match");
