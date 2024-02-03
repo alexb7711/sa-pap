54128c5df1bb9b8f22321fefe642fb9c2227c196
Alexander Brown <alex.brown7711@gmail.com>
2024-02-03 12:03:47 -0700

    Don't attempt to draw real-time plot when flag is low

--- src/plotter/schedule_plot.rs
+++ src/plotter/schedule_plot.rs
@@ -197,14 +197,11 @@ impl Plotter for SchedulePlot {
             // Clear plots
             fg_slow.clear_axes();
             fg_fast.clear_axes();
-        }
 
-        // Create plot
-        SchedulePlot::create_plot(d, fg_slow, fg_fast);
-
-        // Plot Figure
-        if display_plot {
+            // Create plot
+            SchedulePlot::create_plot(d, fg_slow, fg_fast);
             // Update plots
+
             fg_slow.show_and_keep_running().unwrap();
             fg_fast.show_and_keep_running().unwrap();
         }
